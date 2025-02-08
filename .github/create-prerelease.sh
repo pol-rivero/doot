fc680711fb7c23ae458c0ea004cbbb61192cc40e
Pol Rivero <65060696+pol-rivero@users.noreply.github.com>
2025-02-08 10:55:32 +0100

    Add confirmation

--- .github/create-prerelease.sh
+++ .github/create-prerelease.sh
@@ -3,6 +3,18 @@
 # Exit on error
 set -e
 
+# Get the message from the latest commit and ask for confirmation
+MESSAGE=$(git log -1 --pretty=%B)
+echo "Do you want to create a pre-release on this commit?"
+echo -e "\033[33m$MESSAGE\033[0m"
+read -p "[y/N]: " -n 1 -r
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo ""
+  echo "Aborting."
+  exit
+fi
+
 TAG_NAME="pre.$(date +'%Y.%m.%d.%H%M')"
 echo "Creating and pushing tag: $TAG_NAME"
 
