22760e33493676da7f130f2ccd6804f614932c12
Pol Rivero <65060696+pol-rivero@users.noreply.github.com>
2025-04-06 21:12:09 +0200

    Set ORIGINAL_PWD for custom commands that use relative paths

--- lib/utils/run_command.go
+++ lib/utils/run_command.go
@@ -15,6 +15,7 @@ func RunCommand(pwd AbsolutePath, command string, args ...string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
+	cmd.Env = append(os.Environ(), "ORIGINAL_PWD="+getOriginalPwd())
 
 	log.Info("Running command: '%s %s' (PWD: %s)", command, strings.Join(args, " "), pwd)
 	return cmd.Run()
@@ -25,4 +26,13 @@ func RunCommandStr(pwd AbsolutePath, commandAndArgsStr string, extraArgs ...stri
 	command := commandAndArgs[0]
 	args := append(commandAndArgs[1:], extraArgs...)
 	return RunCommand(pwd, command, args...)
+}
+
+func getOriginalPwd() string {
+	originalPwd, err := os.Getwd()
+	if err != nil {
+		log.Warning("Failed to get current working directory: %v, ORIGINAL_PWD won't be set", err)
+		return ""
+	}
+	return originalPwd
 }
--- test/integration_custom_command_test.go
+++ test/integration_custom_command_test.go
@@ -21,6 +21,21 @@ func TestCustomCmd_RunCommand(t *testing.T) {
 	assert.Equal(t, "hello "+sourceDir()+" arg1 arg2\n", beforeContent)
 }
 
+func TestCustomCmd_OriginalPwd(t *testing.T) {
+	config := config.DefaultConfig()
+	config.ImplicitDot = false
+	setUpFiles_TestCustomCmd(t, config)
+	createCustomCommandFile("my-command", `#!/bin/bash
+		echo "$ORIGINAL_PWD" >> output.txt`)
+
+	lib.ExecuteRootCmd(nil, []string{"my-command", "arg1", "arg2"})
+	beforeContent := readFile(sourceDir() + "/output.txt")
+	// We can't control the location where the repo is cloned, but we know that
+	// go test will run in the same directory as the test file, which in this case
+	// is 'test'. Additionally, we expect an absolute path.
+	assert.Regexp(t, `^/.*/test\n$`, beforeContent)
+}
+
 func TestCustomCmd_CommandFail(t *testing.T) {
 	log.PanicInsteadOfExit = true
 	config := config.DefaultConfig()
