f3f4b0de70b4e9f8cfceb50f1a53d423be7616ba
Pol Rivero <65060696+pol-rivero@users.noreply.github.com>
2025-05-18 18:29:40 +0200

    Homebrew install now working

--- .github/doot.homebrew-formula.template.rb
+++ .github/doot.homebrew-formula.template.rb
@@ -4,62 +4,37 @@ class Doot < Formula
   license  "MIT"
   version  "{{VERSION}}"
   head     "https://github.com/pol-rivero/doot.git", branch: "main"
-  url      "https://github.com/pol-rivero/doot/archive/refs/tags/{{VERSION}}.tar.gz"
-  sha256   "{{TARBALL_CHECKSUM}}"
 
   depends_on "git"
   depends_on "git-crypt"
-  depends_on "go" => :build
-
 
   on_macos do
     on_arm do
-      resource "binary" do
-        url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-darwin-arm64"
-        sha256 "{{DARWIN_ARM64_CHECKSUM}}"
-      end
+      url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-darwin-arm64"
+      sha256 "{{DARWIN_ARM64_CHECKSUM}}"
     end
     on_intel do
-      resource "binary" do
-        url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-darwin-x86_64"
-        sha256 "{{DARWIN_X86_CHECKSUM}}"
-      end
+      url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-darwin-x86_64"
+      sha256 "{{DARWIN_X86_CHECKSUM}}"
     end
   end
 
   on_linux do
     on_arm do
-      resource "binary" do
-        url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-linux-arm64"
-        sha256 "{{LINUX_ARM64_CHECKSUM}}"
-      end
+      url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-linux-arm64"
+      sha256 "{{LINUX_ARM64_CHECKSUM}}"
     end
     on_intel do
-      resource "binary" do
-        url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-linux-x86_64"
-        sha256 "{{LINUX_X86_CHECKSUM}}"
-      end
+      url "https://github.com/pol-rivero/doot/releases/download/{{VERSION}}/doot-linux-x86_64"
+      sha256 "{{LINUX_X86_CHECKSUM}}"
     end
-  end
-
-  resource "source" do
-    url "https://github.com/pol-rivero/doot/archive/refs/tags/{{VERSION}}.tar.gz"
-    sha256 "{{TARBALL_CHECKSUM}}"
   end
 
 
   def install
-    if build.from_source?
-      resource("source").stage do
-        system "go", "build", "-o", "doot", "-trimpath", "-mod=readonly", "-modcacherw"
-        bin.install "doot"
-      end
-    else
-      resource("binary").stage do
-        # rename the downloaded binary to "doot" and install
-        bin.install Dir["doot*"] => "doot"
-      end
-    end
+    mv Dir["doot-*"].first, "doot"
+    chmod 0755, "doot"
+    bin.install "doot"
 
     generate_completions_from_executable(bin/"doot", "completion")
   end
